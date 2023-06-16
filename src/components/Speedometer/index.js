8e483f95cc59f8a5f981f3aae11a41781e6d3c0b
sravankumar <cheripellysravan0411@gmail.com>
2023-06-16 10:25:58 +0000

    final code

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,22 +1,29 @@
 import {Component} from 'react'
+
 import './index.css'
 
 class Speedometer extends Component {
   state = {speed: 0}
+
   onAccelerate = () => {
     const {speed} = this.state
+
     if (speed < 200) {
       this.setState(prevState => ({speed: prevState.speed + 10}))
     }
   }
+
   onBrake = () => {
     const {speed} = this.state
+
     if (speed > 0) {
       this.setState(prevState => ({speed: prevState.speed - 10}))
     }
   }
+
   render() {
     const {speed} = this.state
+
     return (
       <div className="container">
         <h1 className="heading">SPEEDOMETER</h1>
@@ -25,15 +32,19 @@ class Speedometer extends Component {
             src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
             alt="speedometer"
             className="image"
-          ></img>
+          />
         </div>
         <h1 className="paragraph">Speed is {speed}mph</h1>
         <p className="description">Min Limit is 0mph, Max Limit is 200mph</p>
         <div>
-          <button className="accelerator-button" onClick={this.onAccelerate}>
+          <button
+            type="button"
+            className="accelerator-button"
+            onClick={this.onAccelerate}
+          >
             Accelerate
           </button>
-          <button className="brake-button" onClick={this.onBrake}>
+          <button type="button" className="brake-button" onClick={this.onBrake}>
             Apply Brake
           </button>
         </div>
